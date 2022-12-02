0281652992ae3c563befe7dc78cdec6ffd2fb22c
christopherayling <chris.ayling.360@gmail.com>
2022-12-02 14:38:22 +1100

    day 1 part 1 and 2 complete

--- 1/part2.cpp
+++ 1/part2.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <numeric>
 
 int main() {
     std::ifstream file("cals.txt");
     std::string str;
-    int global_max = 0;
+    std::vector<int> cals;
     int current_total = 0;
-    int i = 0;
-    int maxi = 0;
     while (std::getline(file, str)) {
         if (str == "") {
-            if (current_total > global_max) {
-                global_max = current_total;
-                maxi = i;
-            }
+            cals.push_back(current_total);
             current_total = 0;
-             i++;
         } else {
              current_total += stoi(str);
         }
     }
-    std::cout << "global max: " << global_max << std::endl;
-    std::cout << "elf: " << maxi+1 << std::endl;
+    std::sort(cals.begin(), cals.end(), std::greater<int>());
+    int top3sum = std::accumulate(cals.begin(), cals.begin()+3, 0);
+    std::cout << "top3 elves total: " << top3sum << std::endl;
 }
